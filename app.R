ff6b7dd04055f243fad36f4cf1f02118271a38e0
Claudio Zanettini <claudio.zanettini@gmail.com>
2019-11-27 19:38:20 -0500

    first graphs

--- app.R
+++ app.R
@@ -4,7 +4,8 @@
 library(shiny)
 library(shinydashboard)
 library(shinyWidgets)
-
+library(ggthemes)
+source("helper.r")
 
 ui <- dashboardPage(
     dashboardHeader(title = "time Racoon"),
@@ -19,33 +20,35 @@ ui <- dashboardPage(
                         "month")
         ),
         
+        dateRangeInput("date_r", start = "2019/10/26", label = "Date range", format = "mm-dd-yyyy"),
+        
+        actionBttn(
+            inputId = "calc",
+            label = "Calculate",
+            style = "unite", 
+            color = "danger",
+            size = "sm"
+        ),
+        
         sidebarMenu(
-            menuItem("plots", tabName = "Plots", icon = icon("chart-bar")),
-            menuItem("tables", tabName = "Tables", icon = icon("table"))
+            menuItem("plots", tabName = "plots", icon = icon("chart-bar")),
+            menuItem("tables", tabName = "tables", icon = icon("table"))
         )
     ),
     dashboardBody(
         tabItems(
-            tabItem("dashboard",
+            tabItem("plots",
                     fluidRow(
-                        valueBoxOutput("rate"),
-                        valueBoxOutput("count"),
-                        valueBoxOutput("users")
-                    ),
-                    fluidRow(
-                        box(
-                            width = 6, status = "info", solidHeader = TRUE,
-                            title = "Sleep",
-                            # bubblesOutput("packagePlot", width = "100%", height = 600)
-                        ),
-                        box(
-                            width = 6, status = "info",
-                            title = "Something else",
-                            tableOutput("packageTable")
-                        )
-                    )
-            ),
-            tabItem("rawdata",
+                    tabBox(
+                            title = "Plots",
+                            width = 12,
+                            # The id lets us use input$tabset1 on the server to find the current tab
+                            id = "tabset1",
+                            tabPanel("Sleep", "First tab content"),
+                            tabPanel("Work", "Tab content 2")
+                    
+            ))),
+            tabItem("tables",
                     numericInput("maxrows", "Rows to show", 25),
                     verbatimTextOutput("rawtable"),
                     downloadButton("downloadCsv", "Download as CSV")
@@ -56,17 +59,72 @@ ui <- dashboardPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+    
+    
+    dat_rc <- reactiveVal()
+    
 
     observeEvent(
         eventExpr = {
             input$data_in
         },
         handlerExpr = {
+            dat_long <- clean_data(dat = readxl::read_excel(input$data_in$datapath))
+            dat_rc(dat_long)
+        }
+    )
+    
+    observeEvent(
+        eventExpr = {
+            input$calc
+        },
+        handlerExpr = {
+            browser()
+            dat_long <- dat_rc() %>% 
+                filter(date_ins >= input$date_r[1],  date_ins <= input$date_r[2])
+            
+           #--------------------------------------------    
+           if (input$dwm == "day") {
+  
+             dat_long %>% 
+               mutate(sleep = sleep/60) %>% 
+               ggplot() +
+                     geom_hline(yintercept = 9, lty = 4, lwd = 0.5) +
+                     scale_y_continuous(limits = c(0,12), breaks = 1:12, labels = 1:12) +
+                     scale_color_brewer(palette = "Dark2") +
+                     geom_label(aes(min(date_ins), y = 9.5), label = "target") +
+                     labs(x = NULL, y = "hours", color = NULL) +
+                     geom_line(aes(date_ins, sleep)) +
+                     geom_point(size = 3, aes(date_ins, sleep, color = day_ins)) 
 
-            dat <- readxl::read_excel(input$data_in$datapath)
-            dat_long <- clean_data(dat = dat)
+             dat_long %>% 
+               mutate(sleep = sleep/60) %>% 
+               ggplot() +
+                   geom_hline(yintercept = 9, lty = 4, lwd = 0.5) +
+                   scale_y_continuous(limits = c(0,12), breaks = 1:12, labels = 1:12) +
+                   scale_color_brewer(palette = "Dark2") +
+                   geom_label(aes(min(date_ins), y = 9.5), label = "target") +
+                   labs(x = "week staring", y = "hours", color = NULL) +
+                   geom_jitter(size = 3, aes(week_year_ins, sleep, color = day_ins), width = 0.2) 
+                     
+             dat_long %>% 
+               mutate(sleep = sleep/60) %>% 
+               ggplot() +
+               geom_hline(yintercept = 9, lty = 4, lwd = 0.5) +
+               scale_y_continuous(limits = c(0,12), breaks = 1:12, labels = 1:12) +
+               scale_color_brewer(palette = "Dark2") +
+               geom_label(aes(min(date_ins), y = 9.5), label = "target") +
+               labs(x = "week staring", y = "hours", color = NULL) +
+               geom_jitter(size = 3, aes(month_year_ins, sleep, color = day_ins), width = 1) +
+               scale_x_date(date_breaks = "1 month")
+               
+               
+           }
+            
+            
             
         }
+        
     )
     
 }
--- helper.R
+++ helper.R
@@ -6,21 +6,43 @@ library(lubridate)
 library(janitor)
 
 
-
+dat <- readxl::read_excel("personal_tt.xlsx")
 clean_data <- function(dat) {
   dat_long <-
     dat %>%
-    mutate_all(~as.character(.)) %>% 
-    pivot_longer(col = 3: ncol(dat), names_to = "date_ins", values_to = "value") %>% 
-    mutate(date_ins = mdy(date_ins)) %>% 
-    filter(!is.na(date_ins), !is.na(activity)) %>% 
+    mutate_all(~ as.character(.)) %>%
+    select(-starts_with("WEEK")) %>% 
+    pivot_longer(
+      col = 3:ncol(.),
+      names_to = "date_ins",
+      values_to = "value"
+    ) %>%
+    mutate(date_ins = mdy(date_ins)) %>%
+    filter(!is.na(date_ins), !is.na(activity)) %>%
     select(-unit) %>%
-    pivot_wider(names_from = activity, values_from = value) %>% 
-    mutate_at(c("get_up", "bed_time"), ~ as.POSIXct(as.numeric(.) * 60*60*24, origin="1899-12-30", tz="GMT")) %>% 
-    mutate_at(vars(-get_up, -bed_time, -date_ins, -parents, -partner, -brother, -family_others, -friends), as.numeric) %>% 
-    clean_names() %>% 
-    mutate(month_year = floor_date(date_ins, "month"), week_year = floor_date(date_ins, "week", week_start = 1) ) 
+    pivot_wider(names_from = activity, values_from = value) %>%
+    mutate_at(
+      c("get_up", "bed_time"),
+      ~ as.POSIXct(as.numeric(.) * 60 * 60 * 24, origin = "1899-12-30", tz = "GMT")
+    ) %>%
+    mutate_at(
+      vars(
+        -get_up,
+        -bed_time,
+        -date_ins,
+        -parents,
+        -partner,
+        -brother,
+        -family_others,
+        -friends
+      ),
+      as.numeric) %>%
+    clean_names() %>%
+    mutate(
+      month_year_ins = floor_date(date_ins, "month"),
+      week_year_ins = floor_date(date_ins, "week", week_start = 1),
+      day_ins = lubridate::wday(date_ins, label = TRUE, week_start = 1)
+    )
 }
-  
 
-  
+
